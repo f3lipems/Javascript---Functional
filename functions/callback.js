0682a9da0690f3cd6ee73f0d79da51ab49aa87dc
Felipe <felipe@visionar.io>
2024-01-09 08:31:04 -0300

    Add callback

---
+++ functions/callback.js
@@ -0,0 +1,15 @@
+function exec(fn, n1, n2) {
+    if (typeof fn === 'function') {
+        fn(n1, n2)
+    }
+}
+
+const addInTerminal = (x, y) => console.log(x + y)
+const subInTerminal = (x, y) => console.log(x - y)
+
+exec(addInTerminal, 56, 38)
+exec(subInTerminal, 182, 27)
+
+
+const logEcec = () => console.log('Exec...');
+setInterval(logEcec, 1000);
