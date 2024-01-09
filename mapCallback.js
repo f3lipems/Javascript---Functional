8be5aa03c7e2a72341d7ec694d75f9013edbad04
Felipe <felipe@visionar.io>
2024-01-09 19:41:36 -0300

    Add maps

---
+++ mapCallback.js
@@ -0,0 +1,21 @@
+const nums = [1, 2, 3, 4, 5]
+const double = (n, i, a) => n * 2 + i + a.length
+console.log(nums.map(double))
+
+const names = ['Ana', 'Bia', 'Rafa', 'Lia, Gui']
+const firstLether = text => text[0]
+console.log(names.map(firstLether))
+
+const cart = [
+    {name: 'Pen', qtt: 10, price: 7.99},
+    {name: 'Printer', qtt: 0, price: 649.59},
+    {name: 'Notebook', qtt: 4, price: 27.10},
+    {name: 'Pencil', qtt: 3, price: 5.82},
+    {name: 'Seasor', qtt: 1, price: 19.20}
+]
+
+const getItems = item => item.name
+const getTotal = item => item.qtt * item.price
+
+console.log(cart.map(getItems))
+console.log(cart.map(getTotal))
