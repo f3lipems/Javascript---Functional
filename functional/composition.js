ff5d6fa481fa230038e02edf9a56793ac8144d8b
Felipe <felipe@visionar.io>
2024-03-21 22:27:06 -0300

    Currying composition

--- functional/composition.js
+++ functional/composition.js
@@ -18,5 +18,11 @@ function lazy(text) {
     return text.split('').join(' ')
 }
 
-const result = composition(scream, enphasize, lazy)('Stop!')
+const forced = composition(scream, enphasize, lazy)
+const lessForced = composition(scream, enphasize)
+
+const result = forced('Stop!')
+const result2 = lessForced('Stop!')
+
 console.log(result)
+console.log(result2)
